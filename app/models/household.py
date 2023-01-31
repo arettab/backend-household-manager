d705709bf32314914175dffc0914895ce177833e
Aretta Brown <aretta.brown@gmail.com>
2023-01-31 09:13:23 -0500

    creates post and get route for user

--- app/models/household.py
+++ app/models/household.py
@@ -3,5 +3,5 @@ from app import db
 class Household(db.Model):
     id = db.Colum(db.Integer, primary_key=True, autoincrement=True)
     name = db.Coulum(db.Sting)
-    members = db.relationship()
+    members = db.relationship("User", secondary="group_members", backref="group")
 
